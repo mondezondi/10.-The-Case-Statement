d7e8f6442466e5973032e51234ac0d0a96cddbbc
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-17 12:40:49 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        ComboBox1 = New ComboBox()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(204, 153)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(129, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter the grade letter"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(280, 208)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(47, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Results"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(246, 268)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Compute!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"A", "B", "C", "D", "E", "F"})
+        ComboBox1.Location = New Point(419, 145)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 3
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(419, 200)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(116, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleMode = AutoScaleMode.Inherit
+        BackColor = Color.FromArgb(CByte(128), CByte(64), CByte(64))
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim grade As String
+        grade = ComboBox1.Text
+
+        Select Case grade
+            Case "A"
+                TextBox1.Text = "High Distinction"
+            Case "A-"
+                TextBox1.Text = "Distinction"
+
+            Case "B"
+                TextBox1.Text = "Credit"
+            Case "C"
+                TextBox1.Text = "Pass"
+
+            Case "D"
+                TextBox1.Text = "Fail"
+            Case Else
+                TextBox1.Text = "Fail"
+
+
+
+
+        End Select
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
